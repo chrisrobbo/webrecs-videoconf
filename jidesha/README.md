96e0c3c4a2296916fef5c357e687292afb9d0ebb
Chris Robinson <chrisr@webrecs.com.au>
2018-10-18 10:34:10 +1100

    Jidesha files

---
+++ jidesha/README.md
@@ -0,0 +1,4 @@
+Jidesha
+=======
+
+Extension for [Firefox](https://github.com/jitsi/jidesha/blob/master/firefox/README.md) and [Chrome](https://github.com/jitsi/jidesha/blob/master/chrome/README.md) for Desktop Sharing in Jitsi Meet.
---
+++ jidesha/chrome-tabcopy/background.js
@@ -0,0 +1,63 @@
+/*
+ * Copyright @ 2015 Atlassian Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Listens for external messages coming from pages that match url pattern
+// defined in manifest.json
+var receiver=null;
+
+chrome.runtime.onMessageExternal.addListener(
+    function(request, sender, sendResponse) {
+        console.log("Got request", request, sender);
+        if(request.getVersion) {
+            sendResponse({ version: chrome.runtime.getManifest().version });
+            return false; // Dispose of sendResponse
+        } else if(request.getStream) {
+            // Desktop video stream sources
+            // Creates hidden tab with URL and creates stream with the content
+        	var video_constraints = {
+        	        mandatory: {
+        	            chromeMediaSource: 'tab'
+        	                }
+        	};
+        	var constraints = {
+                    audio: false,
+                    video: true,
+                   videoConstraints: video_constraints
+                };
+            
+            var startUrl="http://webrecs.com.au";
+
+        	chrome.tabCapture.captureOffscreenTab(startUrl, constraints, function(stream) {
+            	//chrome.tabCapture.capture( constraints, function(streamId) {
+                	console.log("In callback, stream=", stream );
+                	console.log ("Getting  tracks :" , stream.getTracks());
+                    if (receiver != null) {
+                       receiver.close();
+                      }
+                      	receiver=window.open('https://jitsi.hosts.webrecs.com/jitsi/single-stream-recorder/receiver.html', "Hidden tab capture" );
+                        receiver.currentStream = stream;
+                    //	receiver.location='receiver.html';
+                      
+                      sendResponse(stream);
+                });
+            return true; // Preserve sendResponse for future use
+        } else {
+            console.error("Unknown request");
+            sendResponse({ error : "Unknown request" });
+            return false;
+        }
+    }
+);
